08d402f5a9cb5850fbce7d4b1255030a394bcb50
Hariprakash3131 <hariprakash3131@gmail.com>
2026-08-09 23:38:58 +0530

    node23

---
+++ 15JS/day-2/checkPosNegZero.js
@@ -0,0 +1,13 @@
+let check=-2
+
+if(check>0){
+    console.log("Pos")
+}
+
+else if(check<0){
+    console.log('Negative')
+}
+
+else if(check==0){
+    console.log('Zero')
+}
---
+++ 15JS/day-2/evenOrOdd.js
@@ -0,0 +1,7 @@
+let number = 25;
+
+if (number % 2 == 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
