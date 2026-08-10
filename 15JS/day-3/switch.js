afc30299aaf0c5e35ad274397dc70d37456b471c
Hariprakash3131 <hariprakash3131@gmail.com>
2026-08-10 13:40:00 +0530

    node25

---
+++ 15JS/day-3/switch.js
@@ -0,0 +1,24 @@
+let month=5
+
+switch(month){
+    case 1:
+        console.log('Jan')
+        break;
+    case 2:
+        console.log('Feb')
+        break;
+    case 3:
+        console.log('Mar')
+        break;
+    case 4:
+        console.log('Apr')
+        break;
+    case 5 :
+        console.log('May')
+        break;
+    case 6:
+        console.log('June')
+        break;
+    default:
+        console.log('Invalid Month')
+}
