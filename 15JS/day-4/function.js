ed65e7b6781934c9958657c4f32dcb3927026210
Hariprakash3131 <hariprakash3131@gmail.com>
2026-09-16 13:24:35 +0530

    node 45

--- 15JS/day-4/function.js
+++ 15JS/day-4/function.js
@@ -12,3 +12,26 @@ function PersonName(name){
 
 PersonName('Hari')
 PersonName('Prakash')
+
+
+
+//Function With Multiple Parameters
+
+function add(a,b){
+    console.log(a+b)
+    console.log(a-b)
+}
+
+add(10,90)
+add(23,90)
+
+
+//Return Statement
+
+function mul(a,b){
+    return a*b
+}
+
+let ans=mul(10,2)
+
+console.log(ans)
